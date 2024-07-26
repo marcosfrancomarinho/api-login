6d4887acb219f06a9a8e4d46579f1ab952dd9d61
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-07-25 22:19:17 -0300

    alteraçoes de cors

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";  // Importa tipos Request e Response do pacote express
-import { QueryDB } from '../database/query'; // Importa a camada de acesso a dados para interagir com o banco de dados
-import { ValidateDatas } from "../error/validate"; // Importa a classe de validação de dados
-import { iResponseQuery, iValidateDatas, iFind } from "../interfaces/interfaces"; // Importa as interfaces usadas
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
+import { QueryDB } from '../database/query';  // Importa a camada de acesso a dados para interagir com o banco de dados
+import { ValidateDatas } from "../error/validate";  // Importa a classe de validação de dados
+import { iResponseQuery, iValidateDatas, iFind } from "../interfaces/interfaces";  // Importa as interfaces usadas
+import jwt from 'jsonwebtoken';  // Importa o pacote jsonwebtoken para gerar tokens JWT
+import dotenv from 'dotenv';  // Importa o pacote dotenv para carregar variáveis de ambiente a partir de um arquivo .env
+
+dotenv.config();  // Carrega as variáveis de ambiente do arquivo .env
+
 // Define um namespace para os controladores de usuário
 export namespace userControllers {
     // Instancia a classe de validação de dados
@@ -53,9 +55,14 @@ export namespace userControllers {
 
             // Chama a função de login do banco de dados e aguarda a resposta
             const response: iResponseQuery<iFind | null> = await QueryDB.login(email, password);
-            const secret:string = process.env.TOKEN_SECRET as string;
-            const token = jwt.sign({ email: email, password: password }, secret);
+
+            // Gera um token JWT com o email e senha do usuário
+            const secret: string = process.env.TOKEN_SECRET as string;
+            const token = jwt.sign({ email, password }, secret);
+
+            // Define o cabeçalho 'authorization-token' com o token gerado
             res.setHeader('authorization-token', token);
+
             // Envia uma resposta JSON indicando que o login foi bem-sucedido
             res.status(200).json(response);
         } catch (error) {
